f200cc15fb9023473f71c37d347c570c0747baf9
Olayinka Bolakale <131858361+olabola1108@users.noreply.github.com>
2023-10-29 09:08:54 -0400

    Update variables.tf

--- admin/variables.tf
+++ admin/variables.tf
@@ -1,13 +1,13 @@
 variable "access_key" {
-  default = "AKIA37IXXML4YXY347HT"
+  default = ""
 }
 
 variable "secret_key" {
-  default = "0IhIlUBPfHMf0vhswgpFZOVvnQytUnjAR6wrIf/b"
+  default = ""
 }
 
 variable "vault_token" {
-  default = "hvs.jWvnW2F0XkWvfqO3o8I5ZcNJ"
+  default = "hvs."
   #default = "Vault_Token"
 }
 
